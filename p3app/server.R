cc659d5595277ec5f10d64a0a28d3b7a8265a5a5
Vic Seng <vic.seng95@gmail.com>
2021-11-23 13:01:04 -0500

    some structure for app

--- p3app/server.R
+++ p3app/server.R
@@ -10,13 +10,27 @@
 
 library(shiny)
 library(tidyverse)
+library(fastDummies)
 
-# TODO: read in data
-# TODO: preliminary data cleaning/formatting
+# https://archive.ics.uci.edu/ml/datasets/Online+Shoppers+Purchasing+Intention+Dataset
+
+theT <- read_csv("../online_shoppers_intention.csv") # the data set as-is
+theModelT <- dummy_cols(
+  theT,
+  select_columns = c("Month", "VisitorType"),
+  remove_first_dummy = FALSE,
+  remove_most_frequent_dummy = FALSE,
+  ignore_na = FALSE,
+  split = TRUE,
+  remove_selected_columns = TRUE
+) # the data set with dummy cols for factor vars
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  
+    
+  observeEvent(input$tabset, {
+    # TODO: update sidebar widgets depending on what tab is open
+  })
     # TODO: graphical plot based on user input with dynamic to allow for stack vs grouped bars if bar
     # TODO: summary based on user input
     # TODO: split data into training and test based on user ratio
--- p3app/ui.R
+++ p3app/ui.R
@@ -7,19 +7,72 @@ shinyUI(
   fluidPage(
 
     # Application title
-    titlePanel("Relay Deal Intelligence"),
+    titlePanel("Ecommerce Purchase Intent"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
       sidebarPanel(
-        # TODO: conditional sidebar widgets for each tab
+        conditionalPanel(
+          condition = "input.tabset == 'About'",
+          p("The About panel sidebar")
+        ),
+        conditionalPanel(
+          condition = "input.tabset == 'Data Exploration'",
+          p("The Data exploration panel sidebar"),
+          radioButtons(
+            "graphType", 
+            strong("Select a graph type"), 
+            choices = list("Histogram" = "hist", "Scatter" = "scatter")
+          ),
+          selectInput(
+            "graphVar", 
+            strong("Select a varible to see its relationship to purchases"),
+            choices = list("a choice" = 1, "another choice" = 2)
+          )
+        ),
+        conditionalPanel(
+          condition = "input.tabset == 'Modeling' & input.modelTabset == 'Modeling Info'",
+          p("The Modeling.info panel sidebar")
+        ),
+        conditionalPanel(
+          condition = "input.tabset == 'Modeling' & input.modelTabset == 'Model Fitting'",
+          p("The Modeling.fitting panel sidebar")
+        ),
+        conditionalPanel(
+          condition = "input.tabset == 'Modeling' & input.modelTabset == 'Prediction'",
+          p("The Modeling.prediction panel sidebar")
+        ),
+        conditionalPanel(
+          condition = "input.tabset == 'Data'",
+          p("The Data panel sidebar")
+        )
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
+          tabsetPanel(
+            id = "tabset",
             tabPanel(
               "About",
               # TODO: purpose of app, explain data & data source & tab purposes & image
+              h2(" About the App"),
+              HTML("<div>
+                      <img src = 'noun_online shopping_1605024.svg' width = '100px' height = 'auto' align = 'left' alt = 'online shopping icon'>
+                      <p> This app provides an interface for the user to explore, analyze, and model ecommerce session behavior and its impact on purchase.</p>
+                      <p>The data set from the UCI Machine Learning repository contains information about the purchase intent behavior of website visitors. It details when they visited, for how long, what kinds of pages were viewed, and whether they ultimately made an ecommerce purchase.</p>
+                    </div>"
+                   ),
+              br(),
+              p("The user can use the following tabs to interact with and gain isight from the data:"),
+              HTML("<ul>
+                     <li>On the <b>Data Exploration</b> page, the user is able to interact with the data through visualizations and tables.</li> 
+                     <li>On the <b>Modeling</b> page, the user is able to customize & train three types of predictive model and use them to predict purchase outcome for new user-entered data.</li> 
+                     <li>On the <b>Data</b> page, the user can view, filter, and interact with the source data.</li>
+                   </ul>"
+                   ),
+              br(),
+              HTML("<p><em>Data Credit: <a href = 'https://archive.ics.uci.edu/ml/datasets/Online+Shoppers+Purchasing+Intention+Dataset'> Online shoppers intention from the UCI Machine Learning Repository</a></em></p>"),
+              HTML("<p><em>Image credit: online shopping by <a href = 'https://thenounproject.com/grega.cresnar'>Gregor Cresnar</a> from the Noun Project</em></p>")
             ),
             tabPanel(
               "Data Exploration",
@@ -27,25 +80,29 @@ shinyUI(
             ),
             tabPanel(
               "Modeling",
-              tabPanel(
-                "Modeling Info",
-                # TODO: explain 3 models, benefits & drawbacks. use mathJax for math explainers
-              ),
-              tabPanel(
-                "Model Fitting",
-                # TODO: split into training & test (user selects ratio) 
-                # TODO: model settings in sidebar (settings/vars/go button)
-                # TODO: display models, report rmse on training, show summaries, show fit on test
-              ),
-              tabPanel(
-                "Prediction",
-                # TODO: allow users to select predictor vals and use model to show resp value
+              tabsetPanel(
+                id = "modelTabset",
+                tabPanel(
+                  "Modeling Info",
+                  # TODO: explain 3 models, benefits & drawbacks. use mathJax for math explainers
+                ),
+                tabPanel(
+                  "Model Fitting",
+                  # TODO: split into training & test (user selects ratio) 
+                  # TODO: model settings in sidebar (settings/vars/go button)
+                  # TODO: display models, report rmse on training, show summaries, show fit on test
+                ),
+                tabPanel(
+                  "Prediction",
+                  # TODO: allow users to select predictor vals and use model to show resp value
+                )
               )
             ),
             tabPanel(
               "Data",
               # TODO: show data set with user subsets and export functionality
             )
+          )
         )
     )
   )
